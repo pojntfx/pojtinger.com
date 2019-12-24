3a2610e8b42c2faf82f8098778e2641edab613a3
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-12-24 11:37:59 +0100

    feat: Add initial structuree

---
+++ src/Home.tsx
@@ -0,0 +1,6 @@
+import * as React from "react"
+
+export const Home = () => <div>
+    <h1>Home</h1>
+    <h2>Subheader</h2>
+</div>;
---
+++ src/Root.tsx
@@ -0,0 +1,6 @@
+import * as React from "react"
+import {Home} from "./Home";
+
+export const Root = () => <>
+    <Home/>
+</>;
