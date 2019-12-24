9d1775ebe45609120c36d347a64159dcf0566a95
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-12-24 13:15:11 +0100

    feat: Add imprint and decomposed data

--- src/PageLayout.tsx
+++ src/PageLayout.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import {Button} from "semantic-ui-react";
+import {Button, Segment} from "semantic-ui-react";
 import {Link} from "react-router-dom"
 import styled from "@emotion/styled";
 import {css, Global} from "@emotion/core"
@@ -32,23 +32,32 @@ const MenuBar = styled.div`
     }
 `;
 
-export const PageLayout = (props: { children: JSX.Element | JSX.Element[], home?: boolean }) => <Wrapper>
-    <Global styles={css`#root {
+const SegmentWrapper = styled(Segment)`
+    min-width: 100%;
+    min-height: 100%;
+    flex-grow: 1;
+`;
+
+export const PageLayout = (props: { children: JSX.Element | JSX.Element[], home?: boolean, segment?: boolean }) =>
+    <Wrapper>
+        <Global styles={css`#root {
         height: 100vh;
         overflow-y: auto;
         > .container {
             height: 100%;
         }
     }`}/>
-    <MenuBar home={props.home}>
-        {!props.home && <Button as={Link} to="/" icon="arrow left" content="Back"/>}
-        <Button as="a" href="https://nx904.your-storageshare.de/" icon="unlock" content="Login"/>
-    </MenuBar>
-    <InnerWrapper>
-        {props.children && props.children}
-    </InnerWrapper>
-    <MenuBar>
-        <Button as="a" href="https://github.com/pojntfx/family-site/" content="© 2019 Felicitas Pojtinger"/>
-        <Button as={Link} to="/imprint" icon="legal" content="Impressum"/>
-    </MenuBar>
-</Wrapper>;
+        <MenuBar home={props.home}>
+            {!props.home && <Button as={Link} to="/" icon="arrow left" content="Back"/>}
+            <Button as="a" href="https://nx904.your-storageshare.de/" icon="unlock" content="Login"/>
+        </MenuBar>
+        <InnerWrapper>
+            {props.segment ? <SegmentWrapper>
+                {props.children && props.children}
+            </SegmentWrapper> : props.children && props.children}
+        </InnerWrapper>
+        <MenuBar>
+            <Button as="a" href="https://github.com/pojntfx/family-site/" content="© 2019 Felicitas Pojtinger"/>
+            <Button as={Link} to="/imprint" icon="legal" content="Impressum"/>
+        </MenuBar>
+    </Wrapper>;
---
+++ src/constants.ts
@@ -0,0 +1,30 @@
+const constants: {
+    people: {
+        name: string
+        path: string
+    }[]
+} = {
+    people: [{
+        name: "Toni",
+        path: "/toni"
+    }, {
+        name: "Luis",
+        path: "/luis"
+    }, {
+        name: "Felicitas",
+        path: "/felicitas"
+    }, {
+        name: "Felicitas",
+        path: "/felicitas"
+    }, {
+        name: "Michael",
+        path: "/michael"
+    }, {
+        name: "Nette",
+        path: "/nette"
+    }]
+};
+
+export {
+    constants
+}
