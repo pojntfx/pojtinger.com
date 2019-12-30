ef64170e9a4d7ce739805dda8715a9cf21261861
Felix Pojtinger <felix@pojtinger.com>
2019-12-30 02:27:59 +0100

    feat: Add name and username display, make footer responsive

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import styled from "@emotion/styled";
 import { IPerson } from ".";
+import { Grid, Header } from "semantic-ui-react";
+import { constants } from "./constants";
 
-const HeaderWrapper = styled.div`
-  display: flex;
-  flex-align: center;
-  justify-content: center;
+const HeaderWrapper = styled(Grid)`
   width: 100%;
 `;
 
@@ -16,9 +15,19 @@ const HeaderAvatar = styled.img`
 `;
 
 export const Person = (props: { person: IPerson }) => (
-  <>
-    <HeaderWrapper>
-      <HeaderAvatar src={props.person.avatarLink} />
-    </HeaderWrapper>
-  </>
+  <HeaderWrapper columns={3} verticalAlign="middle" stackable>
+    <Grid.Row>
+      <Grid.Column verticalAlign="middle" textAlign="center">
+        <Header
+          as="h1"
+          content={`${props.person.name} ${constants.familyName}`}
+          subheader={props.person.username}
+        />
+      </Grid.Column>
+      <Grid.Column verticalAlign="middle" textAlign="center">
+        <HeaderAvatar src={props.person.avatarLink} />
+      </Grid.Column>
+      <Grid.Column verticalAlign="middle" textAlign="center"></Grid.Column>
+    </Grid.Row>
+  </HeaderWrapper>
 );
--- src/constants.ts
+++ src/constants.ts
@@ -3,21 +3,26 @@ interface Person {
   path: string;
   remoteLink?: string;
   avatarLink: string;
+  username: string;
 }
 
 const constants: {
+  familyName: string;
   parents: Person[];
   children: Person[];
 } = {
+  familyName: "Pojtinger",
   parents: [
     {
       name: "Nette",
+      username: "@an.nette.pojtinger",
       path: "/nette",
       avatarLink:
         "https://scontent-frt3-1.cdninstagram.com/v/t51.2885-19/s320x320/36986286_2142786699331022_1530632417654603776_n.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_ohc=3Fxq7qeP95MAX-QJWaJ&oh=961ade99e7c780dd187fbc200ff42c67&oe=5E9B885A"
     },
     {
       name: "Michael",
+      username: "@michaelpojtinger",
       path: "/michael",
       avatarLink:
         "https://scontent-frt3-1.cdninstagram.com/v/t51.2885-19/s150x150/13381258_259063154456746_965134386_a.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_ohc=bJO_AC83qioAX-7deWB&oh=ca2c3f7b4f2258d7c3665fc283e96522&oe=5E8D2F72"
@@ -26,18 +31,21 @@ const constants: {
   children: [
     {
       name: "Toni",
+      username: "@toni.pojtinger",
       path: "/toni",
       avatarLink:
         "https://scontent-frt3-1.cdninstagram.com/v/t51.2885-19/s320x320/69318062_511613642922670_4868879376055271424_n.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_ohc=1A2_3t1c1jYAX-Nq3sI&oh=94dd56862fac9b1179ff951577a6174b&oe=5EA00918"
     },
     {
       name: "Luis",
+      username: "@luispojtinger",
       path: "/luis",
       avatarLink:
         "https://scontent-frt3-1.cdninstagram.com/v/t51.2885-19/s320x320/57625259_2211766638936320_6528318777744949248_n.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_ohc=j8kaFl1Ir60AX8W8zZe&oh=3d34c898428bbacd0cad9c99f65562c9&oe=5E98FD2E"
     },
     {
       name: "Felix",
+      username: "@pojntfx",
       path: "/felix",
       remoteLink: "https://felix.pojtinger.com/",
       avatarLink:
